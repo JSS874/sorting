46d2a8891aa249adc44e8d6c05987a9458548cda
Jayden <jaydenspitulnik@gmail.com>
2022-09-24 18:32:35 -0400

    shapes

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function createList(size) {
   }
   console.log(nums);
   document.getElementById("display").innerHTML = nums;
+  drawShapes();
 }
 
 async function bubbleSort(nums) {
@@ -27,12 +28,45 @@ async function bubbleSort(nums) {
         var temp = nums[j + 1];
         nums[j + 1] = nums[j];
         nums[j] = temp;
-        document.getElementById('display').innerHTML = nums;
+        document.getElementById("display").innerHTML = nums;
+        drawShapes();
       }
-      await new Promise(res => setTimeout(res, 10));
+      await new Promise((res) => setTimeout(res, 10));
     }
     //console.log("Pass");
   }
   //document.getElementById("display").innerHTML = nums;
   console.log(nums);
+}
+
+const canvas = document.getElementById("area");
+const ctx = canvas.getContext("2d");
+
+//ctx.fillRect(55,55,100,100);
+
+function drawShapes() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var width = window.innerWidth / nums.length;
+  var x = 0;
+  let colors = new Map([
+    [1, "red"],
+    [2, "blue"],
+    [3, "green"],
+    [4, "orange"],
+    [5, "yellow"]
+  ])
+  var color = 1;
+  //ctx.fillStyle = "red";
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 5;
+  for (var i = 0; i < nums.length - 1; i++) {
+    ctx.fillStyle = colors.get(color);
+    color++;
+    if (color == 6) {color = 1;}
+    //ctx.setTransform(1, 0, 0, -1, 0, nums[i]);
+    ctx.fillRect(x, 55, 20, nums[i]);
+    //ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.stroke();
+    x += 20;
+  }
 }
